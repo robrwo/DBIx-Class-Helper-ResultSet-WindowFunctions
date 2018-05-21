41a2206766fb5e3e93c8db43660ec7a1888cf5ad
Robert Rothenberg <rrwo@cpan.org>
2018-05-22 00:16:43 +0100

    Add initial prototype to the repo

---
+++ lib/DBIx/Class/Helper/ResultSet/WindowFunctions.pm
@@ -0,0 +1,60 @@
+package DBIx::Class::Helper::ResultSet::WindowFunctions;
+
+use strict;
+use warnings;
+
+use parent 'DBIx::Class::ResultSet';
+
+sub _resolved_attrs {
+    my $rs    = $_[0];
+    my $attrs = $rs->{attrs};
+
+    my $sqla = $rs->result_source->storage->sql_maker;
+
+    foreach my $attr (qw/ select +select /) {
+
+        my $sel = $attrs->{$attr} or next;
+        my @sel;
+
+        foreach my $col ( @{ ref $sel eq 'ARRAY' ? $sel : [$sel] } ) {
+
+            push @sel, $col;
+
+            next unless ref $col eq 'HASH';
+
+            my $as = delete $col->{'-as'};
+            my $over = delete $col->{'-over'} or next;
+
+            $rs->throw_exception('-over must be a hashref')
+              unless ref $over eq 'HASH';
+
+            my ( $sql, @bind ) = $sqla->_recurse_fields($col);
+
+            my ( $part_sql, @part_bind ) =
+              $sqla->_recurse_fields( $over->{partition_by} );
+            if ($part_sql) {
+                $part_sql = $sqla->_sqlcase('partition by ') . $part_sql;
+            }
+
+            my ( $order_sql, @order_bind ) =
+              $sqla->_order_by( $over->{order_by} );
+
+            $sql .= $sqla->_sqlcase(' over (') . $part_sql . $order_sql . ')';
+            if ($as) {
+                $sql .= $sqla->_sqlcase(' as ') . $sqla->_quote($as);
+            }
+
+            push @bind, @part_bind, @order_bind;
+
+            $sel[-1] = \[ $sql, @bind ];
+
+        }
+
+        $attrs->{$attr} = \@sel;
+
+    }
+
+    return $rs->next::method;
+}
+
+1;
